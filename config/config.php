2cbce9babe61ccb4120c6a04460481be67e8b981
judvelasco29 <201980205@psu.palawan.edu.ph>
2022-03-18 00:41:54 +0800

    logApp Velasco done

---
+++ config/config.php
@@ -0,0 +1,6 @@
+<?php
+	define('ROOT_URL', '');
+	define('DB_HOST', 'sql6.freesqldatabase.com');
+	define('DB_USER', 'sql6479627');
+	define('DB_PASS', 'DN6Is7f3ii');
+	define('DB_NAME', 'sql6479627');
