2479e11ee0d479b0f1d23e48fcba666274c55dec
judvelasco29 <201980205@psu.palawan.edu.ph>
2022-04-20 19:32:20 +0800

    commit changes

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 	define('ROOT_URL', '');
-	define('DB_HOST', 'sql6.freesqldatabase.com');
-	define('DB_USER', 'sql6479627');
-	define('DB_PASS', 'DN6Is7f3ii');
-	define('DB_NAME', 'sql6479627');
+	define('DB_HOST', 'localhost');
+	define('DB_USER', 'root');
+	define('DB_PASS', '');
+	define('DB_NAME', 'logApp');
--- inc/footer.php
+++ inc/footer.php
@@ -1,7 +1,13 @@
 <footer class="mastfoot mt-auto" style="text-align: center;">
     <div class="inner">
-      <p>Created by <a href="https://github.com/KDimla">kjmartinez</a> for BSIT 3B1</p>
+      <p>Created by <a href="https://github.com/judVelasco29">judVelasco29</a> for BSIT 3B1</p>
+      <p>Members</p>
+      <p><a href="https://github.com/KDimla">Dimla, Kurt</a></p>
+      <p><a href="https://github.com/JLouEncinas">Encinas, Jonny Lou</a></p>
+      <p><a href="https://github.com/judVelasco29">Velasco, Judy Mea</a></p>
+
     </div>
+
 </footer>
   </body>
 </html>
